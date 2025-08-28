5038c28863b95719b5c652df24f9a318392b6cd2
cepiyogaasmara <cepiyoga@gmail.com>
2025-08-28 21:08:37 +0700

    Commit ke2

--- resources/js/Pages/HalamanUtama/Sales/Sales1.jsx
+++ resources/js/Pages/HalamanUtama/Sales/Sales1.jsx
@@ -1,13 +1,34 @@
 import Layout from "../../../layouts/Layout.jsx";
 import {Head} from "@inertiajs/react";
+import Bar from "../../../components/Bar.jsx";
 
 
 function Home() {
     return (
-        <div className={`flex`}>
+        <>
             <Head title="Sales1"/>
-            <h1 className={`mx-auto text-4xl`}>Sales 1</h1>
-        </div>
+            <div>
+
+                <h1 className={`mx-auto text-4xl text-center`}>Sales 1</h1>
+                <div className={`grid grid-cols-2 gap-4`}>
+                    <div className={` h-96`}>
+                        <Bar/>
+                    </div>
+
+                    <div className={` h-96`}>
+                        <Bar/>
+                    </div>
+
+                    <div className={` h-96`}>
+                        <Bar/>
+                    </div>
+
+                    <div className={` h-96`}>
+                        <Bar/>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
